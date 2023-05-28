2242de2a020be8eda054af8331a920c587de270d
ferencprem2 <fercsusz0508@gmail.com>
2023-05-28 23:09:11 +0200

    Add project files.

---
+++ Dal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroGUI
+{
+    internal class Music
+    {
+        private int ev;
+        private string eloado;
+        private string cim;
+        private int helyezes;
+        private int pontszam;
+
+        public Music(int ev, string eloado, string cim, int helyezes, int pontszam) 
+        {
+            this.ev = ev;
+            this.eloado = eloado;
+            this.cim = cim;
+            this.helyezes = helyezes;
+            this.pontszam = pontszam;
+
+        }
+
+        public int Ev { get; set; }
+        public string Eloado { get; set; }
+        public string Cim { get; set; }
+        public int Helyezes { get; set; }
+        public int Pontszam { get; set; }
+    }
+}
